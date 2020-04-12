ffe63b588ecee9bf7de2f838a7a8041b9c2ebd2d
Lekan Ojulowo <lekanojulowo@gmail.com>
2020-04-12 15:26:06 +0100

    fix logs

--- src/estimator.js
+++ src/estimator.js
@@ -9,7 +9,6 @@ const covid19ImpactEstimator = (data) => {
   let { timeToElapse } = data;
 
   // Custom Functions and Variables
-
   // normalize days; check for weeks and months if used
   if (periodType === 'months') timeToElapse = Math.trunc(timeToElapse * 30);
   if (periodType === 'weeks') timeToElapse = Math.trunc(timeToElapse * 7);
@@ -93,5 +92,4 @@ const covid19ImpactEstimator = (data) => {
     severeImpact // your severe case estimation
   };
 };
-
 export default covid19ImpactEstimator;
